6ce186535f6a56d1647fd333881b75d622045311
Pranay Waghmare <pranayw2300@gmail.com>
2026-03-06 16:11:01 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,6 +1,9 @@
 locals {
   alb_ingress_ports = [80]
+
   ec2_ingress_ports = [22]
+
+  ec2_http_ports = [80]
 }
 
 resource "aws_security_group" "alb_sg" {
@@ -34,15 +37,19 @@ resource "aws_security_group" "ec2_sg" {
   name   = "ec2-sg"
   vpc_id = aws_vpc.devops_vpc.id
 
-  # Allow HTTP only from ALB
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+  # HTTP from ALB
+  dynamic "ingress" {
+    for_each = local.ec2_http_ports
+
+    content {
+      from_port       = ingress.value
+      to_port         = ingress.value
+      protocol        = "tcp"
+      security_groups = [aws_security_group.alb_sg.id]
+    }
   }
 
-  # SSH access for debugging
+  # SSH access
   dynamic "ingress" {
     for_each = local.ec2_ingress_ports
 
