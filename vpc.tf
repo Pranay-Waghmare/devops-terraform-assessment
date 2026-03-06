0f134f904e08745ed339807356f3ffeb438b2a96
Pranay Waghmare <pranayw2300@gmail.com>
2026-03-06 14:13:42 +0530

    Add VPC and related resources configuration

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,15 @@
+resource "aws_vpc" "devops_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "devops-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.devops_vpc.id
+}
+
 resource "aws_subnet" "public1" {
   vpc_id                  = aws_vpc.devops_vpc.id
   cidr_block              = var.public_subnet_1_cidr
@@ -22,4 +34,24 @@ resource "aws_subnet" "private2" {
   vpc_id            = aws_vpc.devops_vpc.id
   cidr_block        = var.private_subnet_2_cidr
   availability_zone = "ap-south-1b"
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.devops_vpc.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public1_assoc" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public2_assoc" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public_rt.id
 }
