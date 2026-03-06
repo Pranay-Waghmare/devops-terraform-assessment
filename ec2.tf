47681c81cec4159be70f14386f2bacb23d038a77
Pranay Waghmare <pranayw2300@gmail.com>
2026-03-06 16:14:51 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
 locals {
   ec2_instances = {
     nginx1 = {
-      subnet_id = aws_subnet.private1.id
-      name      = "nginx-private-1"
+      subnet_id = aws_subnet.public1.id
+      name      = "nginx-public-1"
     }
 
     nginx2 = {
-      subnet_id = aws_subnet.private2.id
-      name      = "nginx-private-2"
+      subnet_id = aws_subnet.public2.id
+      name      = "nginx-public-2"
     }
   }
 }
@@ -19,6 +19,8 @@ resource "aws_instance" "nginx" {
   instance_type = var.instance_type
 
   subnet_id = each.value.subnet_id
+
+  associate_public_ip_address = true
 
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
