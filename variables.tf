cf246d5a015afdc577fa31e5cdaef5983f5dbbc9
Pranay Waghmare <pranayw2300@gmail.com>
2026-03-06 14:09:19 +0530

    Add variables for Terraform configuration

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {}
+variable "vpc_cidr" {}
+
+variable "public_subnet_1_cidr" {}
+variable "public_subnet_2_cidr" {}
+
+variable "private_subnet_1_cidr" {}
+variable "private_subnet_2_cidr" {}
+
+variable "instance_type" {}
+variable "ami_id" {}
