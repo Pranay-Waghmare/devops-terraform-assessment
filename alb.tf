7787c328ac5bf5ba03befb9f119389b161f8543e
Pranay Waghmare <pranayw2300@gmail.com>
2026-03-06 15:17:12 +0530

    Add health check to nginx target group
    Add health check configuration to nginx target group

--- alb.tf
+++ alb.tf
@@ -16,6 +16,17 @@ resource "aws_lb_target_group" "tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.devops_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
 resource "aws_lb_target_group_attachment" "tg_attach" {
