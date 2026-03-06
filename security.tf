ec0ff88870d88f13786d5dddcbf3b006c44f7fb3
Pranay Waghmare <pranayw2300@gmail.com>
2026-03-06 15:53:35 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,12 +1,21 @@
+locals {
+  alb_ingress_ports = [80]
+  ec2_ingress_ports = [22]
+}
+
 resource "aws_security_group" "alb_sg" {
   name   = "alb-sg"
   vpc_id = aws_vpc.devops_vpc.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = local.alb_ingress_ports
+
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
   egress {
@@ -14,6 +23,10 @@ resource "aws_security_group" "alb_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
   }
 }
 
@@ -21,6 +34,7 @@ resource "aws_security_group" "ec2_sg" {
   name   = "ec2-sg"
   vpc_id = aws_vpc.devops_vpc.id
 
+  # Allow HTTP only from ALB
   ingress {
     from_port       = 80
     to_port         = 80
@@ -28,10 +42,26 @@ resource "aws_security_group" "ec2_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }
 
+  # SSH access for debugging
+  dynamic "ingress" {
+    for_each = local.ec2_ingress_ports
+
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
   }
 }
